73e81b3c17c4362c10131f4856f01d9a5aef11a7
nergantre <nergantre@mail.com>
2015-08-20 13:09:35 -0700

    npc: JSONLoader: load traits and attributes

--- nightgames/characters/custom/JSONSourceNPCDataLoader.java
+++ nightgames/characters/custom/JSONSourceNPCDataLoader.java
@@ -3,16 +3,32 @@ package nightgames.characters.custom;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import nightgames.characters.Plan;
+import nightgames.characters.Trait;
+import nightgames.characters.Attribute;
 import nightgames.items.Clothing;
 import nightgames.items.Item;
 
 public class JSONSourceNPCDataLoader {
+	
+	private static int readNumber(JSONObject struct, String key) {
+		return ((Number)struct.get(key)).intValue();
+	}
+	
+	private static void loadResources(JSONObject resources, Stats stats) {
+		stats.stamina = readNumber(resources, "stamina");
+		stats.arousal = readNumber(resources, "arousal");
+		stats.mojo = readNumber(resources, "mojo");
+		stats.willpower = readNumber(resources, "willpower");
+	}
+
 	public static NPCData load(String path) throws ParseException,IOException {
 		Object value = JSONValue.parse(new FileReader(path));
 		DataBackedNPCData data = new DataBackedNPCData();
@@ -21,6 +37,8 @@ public class JSONSourceNPCDataLoader {
 			data.name = (String) object.get("name");
 			data.trophy = Item.valueOf((String)object.get("trophy"));
 			data.plan = Plan.valueOf((String)object.get("plan"));
+			
+			//load outfit
 			JSONObject outfit = (JSONObject) object.get("outfit");
 			JSONArray top = (JSONArray) outfit.get("top");
 			for (Object clothing : top) {
@@ -30,6 +48,20 @@ public class JSONSourceNPCDataLoader {
 			for (Object clothing : bottom) {
 				data.bottom.push(Clothing.valueOf((String)clothing));
 			}
+			
+			// load stats
+			JSONObject stats = (JSONObject)object.get("stats");
+			// load base stats
+			JSONObject baseStats = (JSONObject)stats.get("base");
+			data.stats.level = ((Number)baseStats.get("level")).intValue();
+			// load attributes
+			JSONObject attributes = (JSONObject)baseStats.get("attributes");
+			for (Object attributeString :attributes.keySet()) {
+				Attribute att = Attribute.valueOf((String)attributeString);
+				data.stats.attributes.put(att, ((Number)attributes.get(attributeString)).intValue());
+			}
+			loadResources((JSONObject)baseStats.get("resources"), data.stats);
+			loadTraits((JSONArray)baseStats.get("traits"), data.stats.traits);
 		} catch (ClassCastException e) {
 			throw new ParseException("Badly formatted JSON character: " + e.getMessage(), 0);
 		} catch (IllegalArgumentException e) {
@@ -37,4 +69,12 @@ public class JSONSourceNPCDataLoader {
 		}
 		return data;
 	}
+
+	private static void loadTraits(JSONArray arr, List<Trait> traits) {
+		for (Object traitString : arr) {
+			Trait trait = Trait.valueOf((String)traitString);
+			traits.add(trait);
+		}
+	}
+
 }
