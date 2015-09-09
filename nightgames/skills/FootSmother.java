f2ccba45f1938da3137d05778b1d0e1c53b97aed
nergantre <nergantre@mail.com>
2015-09-09 09:13:27 -0700

    skill: no longer require reach bottom for foot smother

--- nightgames/skills/FootSmother.java
+++ nightgames/skills/FootSmother.java
@@ -25,7 +25,7 @@ public class FootSmother extends Skill {
 
 	@Override
 	public boolean usable(Combat c, Character target) {
-		return getSelf().footAvailable()&&getSelf().body.has("feet")&&c.getStance().reachBottom(target)&&getSelf().canAct()&&c.getStance().prone(target)&&!c.getStance().behind(getSelf());
+		return getSelf().footAvailable()&&getSelf().body.has("feet")&&getSelf().canAct()&&c.getStance().prone(target)&&!c.getStance().behind(getSelf());
 	}
 
 	public int accuracy(Combat c){
