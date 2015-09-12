bab5d8c61d256ba2d6ba63cce1d2a89f4566c84e
nergantre <nergantre@mail.com>
2015-09-12 09:55:53 -0700

    text: fix unformatted text in reversing missionary

--- nightgames/stance/Missionary.java
+++ nightgames/stance/Missionary.java
@@ -89,12 +89,12 @@ public class Missionary extends MaledomSexStance {
 	public Position reverse(Combat c) {
 		boolean coiled = Global.random(2) == 0;
 		if (coiled) {
-		c.write(bottom, Global.format("self:SUBJECT-ACTION:wrap|wraps} {self:possessive} legs around {other:name-possessive} waist and suddenly {self:action:pull|pulls} {other:direct-object} into a deep kiss. {other:SUBJECT-ACTION:are|is} so surprised by this sneak attack that {other:subject-action:don't|doesn't} "
+		c.write(bottom, Global.format("{self:SUBJECT-ACTION:wrap|wraps} {self:possessive} legs around {other:name-possessive} waist and suddenly {self:action:pull|pulls} {other:direct-object} into a deep kiss. {other:SUBJECT-ACTION:are|is} so surprised by this sneak attack that {other:subject-action:don't|doesn't} "
 				+ "even notice {self:pronoun} {self:action:rolling|rolling} {other:direct-object} onto {other:possessive} back until {other:subject-action:feel|feels} {self:possessive} weight on {other:possessive} hips. {self:PRONOUN} {self:action:move|moves} {self:possessive} hips experimentally, enjoying the control "
 				+ "{self:pronoun} {self:action:have|has} in cowgirl position.", bottom, top));
 		return new Cowgirl(bottom, top);
 		} else {
-			c.write(bottom, Global.format("self:SUBJECT-ACTION:wrap|wraps} {self:possessive} legs around {other:name-possessive} waist and suddenly {self:action:pull|pulls} {other:direct-object} into a deep kiss. {other:SUBJECT-ACTION:are|is} so surprised by this sneak attack that {other:subject-action:don't|doesn't} "
+			c.write(bottom, Global.format("{self:SUBJECT-ACTION:wrap|wraps} {self:possessive} legs around {other:name-possessive} waist and suddenly {self:action:pull|pulls} {other:direct-object} into a deep kiss. {other:SUBJECT-ACTION:are|is} so surprised by this sneak attack that {other:subject-action:don't|doesn't} "
 					+ "even notice {self:reflective} getting trapped until {other:subject-action:feel|feels} {self:possessive} limbs wrapped around {other:possessive} body. {self:PRONOUN} {self:action:smile|smiles} widely, enjoying the control "
 				+ "{self:pronoun} {self:action:have|has} coiled around {other:direct-object}.", bottom, top));
 			return new Cowgirl(bottom, top);
