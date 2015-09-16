2ef69dbe549319a5a8831c2831fdd7bcd6b1ab3f
nergantre <nergantre@mail.com>
2015-09-16 15:15:28 -0700

    npc: add portrait for samantha

--- nightgames/global/Global.java
+++ nightgames/global/Global.java
@@ -150,8 +150,8 @@ public class Global {
 		debug[DebugFlags.DEBUG_SCENE.ordinal()] = true;
 		debug[DebugFlags.DEBUG_LOADING.ordinal()] = true;
 //		debug[DebugFlags.DEBUG_DAMAGE.ordinal()] = true;
-		debug[DebugFlags.DEBUG_SKILLS.ordinal()] = true;
-		debug[DebugFlags.DEBUG_SKILLS_RATING.ordinal()] = true;
+//		debug[DebugFlags.DEBUG_SKILLS.ordinal()] = true;
+//		debug[DebugFlags.DEBUG_SKILLS_RATING.ordinal()] = true;
 //		debug[DebugFlags.DEBUG_PLANNING.ordinal()] = true;
 //		debug[DebugFlags.DEBUG_SKILL_CHOICES.ordinal()] = true;
 		traitRequirements = new TraitTree(ResourceLoader.getFileResourceAsStream("data/TraitRequirements.xml"));
