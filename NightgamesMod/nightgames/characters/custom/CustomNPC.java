2c65da7f67f1b4f9607efd08b396596a9b6f09c5
xayaz <xayazzayax@gmail.com>
2016-02-14 11:45:24 -0800

    remove unused import

--- NightgamesMod/nightgames/characters/custom/CustomNPC.java
+++ NightgamesMod/nightgames/characters/custom/CustomNPC.java
@@ -2,7 +2,6 @@ package nightgames.characters.custom;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
