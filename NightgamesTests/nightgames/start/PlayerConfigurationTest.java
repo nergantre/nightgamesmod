d0a1774ae89977b8b824073e844af932fb81b4f3
Ryplinn <ryplinn@gmail.com>
2016-06-20 16:49:47 -0700

    Fixed bug where configuring a character's level would change their money instead.

---
+++ NightgamesTests/nightgames/start/PlayerConfigurationTest.java
@@ -0,0 +1,53 @@
+package nightgames.start;
+
+import nightgames.characters.Attribute;
+import nightgames.characters.CharacterSex;
+import nightgames.characters.Player;
+import nightgames.characters.Trait;
+import nightgames.global.JSONUtils;
+import nightgames.items.clothing.Clothing;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests involving starting player configuration.
+ * Created by Ryplinn on 6/20/2016.
+ */
+public class PlayerConfigurationTest {
+    StartConfiguration startConfig;
+    PlayerConfiguration playerConfig;
+
+    @BeforeClass public static void setUpNpcConfigurationTest() {
+        Clothing.buildClothingTable();
+    }
+
+    @Before public void setUp() throws Exception {
+        Path file = new File("NightgamesTests/nightgames/start/TestStartConfig.json").toPath();
+        startConfig = StartConfiguration.parse(JSONUtils.rootFromFile(file));
+        playerConfig = startConfig.player;
+
+    }
+
+    @Test public void testPlayerCreation() throws Exception {
+        Map<Attribute, Integer> chosenAttributes = new HashMap<>();
+        chosenAttributes.put(Attribute.Power, 5);
+        chosenAttributes.put(Attribute.Seduction, 6);
+        chosenAttributes.put(Attribute.Cunning, 7);
+        Player malePlayer = new Player("dude", CharacterSex.male, Optional.of(playerConfig), chosenAttributes);
+        assertEquals(5, malePlayer.level);
+        assertEquals(15000, malePlayer.money);
+        List<Trait> configTraits = playerConfig.traits.get();
+        Collections.sort(configTraits);
+        Collections.sort(malePlayer.traits);
+        assertArrayEquals(configTraits.toArray(), malePlayer.traits.toArray());
+
+    }
+}
